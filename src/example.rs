fabdef4e9231bb11d1e9fdbb7a9a376eaa2f4cb5
MSBarbieri <matheussouzabarbieri@gmail.com>
2023-01-31 12:07:38 -0300

    example to start quickly

---
+++ src/example.rs
@@ -0,0 +1,12 @@
+// use std::collections::{HashMap, HashSet};
+// use std::cell::RefCell;
+// use std::rc::Rc;
+
+pub struct Solution;
+
+use crate::BTree as BT;
+type TreeNode = BT<i32>;
+use crate::ListNode as LL;
+type ListNode = LL<i32>;
+
+//--------------------------------------------------------------------
