cb9bad9ac0dcc74d195aa6dc3cf9a5cebfe6b953
MSBarbieri <matheussouzabarbieri@gmail.com>
2023-03-17 20:24:10 -0300

    update leetcode version

--- flake.nix
+++ flake.nix
@@ -9,14 +9,16 @@
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    utils,
-    naersk,
-    fenix,
-  }:
-    utils.lib.eachDefaultSystem (system: let
+  outputs =
+    { self
+    , nixpkgs
+    , utils
+    , naersk
+    , fenix
+    ,
+    }:
+    utils.lib.eachDefaultSystem (system:
+    let
       pkgs = import nixpkgs {
         inherit system;
         overlays = [
@@ -24,45 +26,46 @@
         ];
       };
 
-      leetcode = pkgs.rustPlatform.buildRustPackage {
-        pname = "leetcode";
-        version = "0.3.12";
+      leetcode = pkgs.rustPlatform.buildRustPackage
+        {
+          pname = "leetcode";
+          version = "0.3.12";
 
-        src = pkgs.fetchgit {
-          url = "https://github.com/MSBarbieri/leetcode-cli.git";
-          rev = "refs/branches/master";
-          sha256 = "sha256-Vp8+55kCX6ban97Pn5Yj6pnCeH2eLU+QbMWvb/+XJWE=";
+          src = pkgs.fetchgit {
+            url = "https://github.com/MSBarbieri/leetcode-cli";
+            rev = "refs/heads/master";
+            sha256 = "sha256-dZv3HftqzelKre1Gd0rGD+gZgqF6YwHYY6DqZ4YLh/8=";
+          };
+
+          cargoSha256 = "sha256-0W116KxulsBibZt1SofwXuEY/4bazdYS1eZCicikQpA=";
+
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ];
+
+          buildInputs = with pkgs;
+            [
+              openssl
+              dbus
+              sqlite
+            ]
+            ++ lib.optionals stdenv.isDarwin [ darwin.apple_sdk.frameworks.Security ];
+
+          meta = with pkgs.lib; {
+            description = "Leet your code in command-line.";
+            homepage = "https://github.com/clearloop/leetcode-cli";
+            licenses = licenses.mit;
+            maintainers = with maintainers; [ congee ];
+            mainProgram = "leetcode";
+          };
         };
 
-        cargoSha256 = "sha256-487xJrQOtgxPHA7MZxoMqb+7iGpLd29WVUiqnbcEuAo=";
-
-        nativeBuildInputs = with pkgs; [
-          pkg-config
-        ];
-
-        buildInputs = with pkgs;
-          [
-            openssl
-            dbus
-            sqlite
-          ]
-          ++ lib.optionals stdenv.isDarwin [darwin.apple_sdk.frameworks.Security];
-
-        meta = with pkgs.lib; {
-          description = "Leet your code in command-line.";
-          homepage = "https://github.com/clearloop/leetcode-cli";
-          licenses = licenses.mit;
-          maintainers = with maintainers; [congee];
-          mainProgram = "leetcode";
-        };
-      };
-
-      naersk-lib = pkgs.callPackage naersk {};
+      naersk-lib = pkgs.callPackage naersk { };
 
       watch_script = pkgs.writeShellScriptBin "watch-script" ''
         #!/bin/bash
         cargo watch -x "fmt" -s "leetcode t $(\cat ./pick_test)"
-     '';
+      '';
       pick_script = pkgs.writeShellScriptBin "pick" ''
         #!/bin/bash
         if [[ -f ./pick_test ]] && [[ -z $1 ]]; then
@@ -73,11 +76,12 @@
             leetcode e $1
           else
             leetcode p | awk 'NR==2 {print $1}' | cut -d "[" -f 2 | cut -d "]" -f 1 > ./pick_test
-          fi 
+          fi
         fi
       '';
 
-    in {
+    in
+    {
       defaultPackage = naersk-lib.buildPackage {
         src = ./.;
         buildInputs = [
@@ -85,7 +89,7 @@
         RUST_LOG = "trace";
       };
 
-     devShell = with pkgs;
+      devShell = with pkgs;
         mkShell {
           buildInputs = [
             (pkgs.fenix.complete.withComponents [
@@ -103,7 +107,7 @@
           ];
 
           RUST_LOG = "debug";
-          nativeBuildInputs = [pkgs.pkg-config];
+          nativeBuildInputs = [ pkgs.pkg-config ];
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
         };
     });
